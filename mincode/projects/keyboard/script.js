b5fa7c519233580fb4abec7b660ce18ef796091f
adrian-gg <garciagregoriadrian@gmail.com>
2025-02-24 01:57:54 +0100

    [update] mincode: keyboard

--- mincode/projects/keyboard/script.js
+++ mincode/projects/keyboard/script.js
@@ -2,47 +2,70 @@ const keyboard = [
   ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
   ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
   ["z", "x", "c", "v", "b", "n", "m"],
-  [" "],
+  [" ", "Backspace"],
 ]
-const keyboardBox = $(".keyboard")
-const outputBox = $(".output")
+const keyboardBox = $("#keyboard")
+const outputBox = $("#output")
 const outputText = []
+let keysBoxes
 
 function printKeyboard() {
   keyboard.forEach((row) => {
     keyboardBox.append(`<div class="row" style="--length-row: ${row.length};">
       ${row
-        .map(
-          (key) =>
-            `<div class="${
-              key == " " ? "key key--s" : "key"
-            }" data-key="${key}">${key}</div>`
-        )
+        .map((key) => {
+          const k = key === "Backspace" ? "⌫" : key
+          return `<div class="${
+            key === " "
+              ? "key key--s"
+              : key === "Backspace"
+              ? "key key--b"
+              : "key"
+          }" data-key="${k}">${k}</div>`
+        })
         .join("")}
     </div>`)
   })
+
+  keysBoxes = $(".key")
 }
 
 printKeyboard()
 
 $(document).on("keydown", function (event) {
-  const key = event.key
-  //console.log(event.keyCode)
-  const keyBox = $('[data-key="' + key + '"]')
-
-  keyBox.toggleClass("key--pressed")
+  const key = event.key === "Backspace" ? "⌫" : event.key
+  const keyBox = $(`[data-key="${key}"]`)
+  keyBox.addClass("key--pressed")
   write(event)
-  setTimeout(() => keyBox.toggleClass("key--pressed"), 100)
+  setTimeout(() => keyBox.removeClass("key--pressed"), 100)
 })
 
-function write(e) {
-  const letter = e.key
-  if (e.keyCode >= 65 && e.keyCode <= 90) {
-    outputText.push(letter)
-  } else if (e.keyCode == 32) {
-    outputText.push("⠀")
-  } else if (e.keyCode == 8) {
-    outputText.pop()
+keysBoxes.on("click", function (event) {
+  const key = event.target.dataset.key
+  const keyBox = $(`[data-key="${key}"]`)
+  keyBox.addClass("key--pressed")
+  write(key, true)
+  setTimeout(() => keyBox.removeClass("key--pressed"), 100)
+})
+
+function write(e, isLetter = false) {
+  if (isLetter) {
+    if (e === " ") {
+      outputText.push(" ")
+    } else if (e === "⌫") {
+      outputText.pop()
+    } else {
+      outputText.push(e)
+    }
+  } else {
+    const letter = e.key
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+      outputText.push(letter)
+    } else if (e.keyCode === 32) {
+      outputText.push(" ")
+    } else if (e.keyCode === 8) {
+      outputText.pop()
+    }
   }
-  outputBox.empty().append(outputText.join(""))
+  outputBox.html(outputText.join("").replaceAll(" ", "&nbsp;"))
 }

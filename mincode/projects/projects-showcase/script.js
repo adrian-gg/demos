dd0f1831c3af6dd1b38ab264a2eff9e08b54a19a
adrian-gg <garciagregoriadrian@gmail.com>
2025-07-21 19:50:59 +0200

    [update] mincode: projects-showcase

--- mincode/projects/projects-showcase/script.js
+++ mincode/projects/projects-showcase/script.js
@@ -3,61 +3,61 @@ const projects = [
     id: 1,
     title: "Silence",
     year: "2021",
-    image: "./img/7d47d4e2-0eff-4e2f-9734-9d24a8ba067e.png",
+    image: "7d47d4e2-0eff-4e2f-9734-9d24a8ba067e",
   },
   {
     id: 2,
     title: "Resonance",
     year: "2022",
-    image: "./img/5eee2d2d-3d4d-4ae5-96d4-cdbae70a2387.png",
+    image: "5eee2d2d-3d4d-4ae5-96d4-cdbae70a2387",
   },
   {
     id: 3,
     title: "Essence",
     year: "2022",
-    image: "./img/def30e8a-34b2-48b1-86e1-07ec5c28f225.png",
+    image: "def30e8a-34b2-48b1-86e1-07ec5c28f225",
   },
   {
     id: 4,
     title: "Void",
     year: "2023",
-    image: "./img/44d7cb23-6759-49e4-9dc1-acf771b3a0d1.png",
+    image: "44d7cb23-6759-49e4-9dc1-acf771b3a0d1",
   },
   {
     id: 5,
     title: "Presence",
     year: "2023",
-    image: "./img/7712fe42-42ca-4fc5-9590-c89f2db99978.png",
+    image: "7712fe42-42ca-4fc5-9590-c89f2db99978",
   },
   {
     id: 6,
     title: "Flow",
     year: "2024",
-    image: "./img/cbee1ec5-01b6-4ffe-9f34-7da7980454cf.png",
+    image: "cbee1ec5-01b6-4ffe-9f34-7da7980454cf",
   },
   {
     id: 7,
     title: "Clarity",
     year: "2024",
-    image: "./img/2e91a9d1-db85-4499-ad37-6222a6fea23b.png",
+    image: "2e91a9d1-db85-4499-ad37-6222a6fea23b",
   },
   {
     id: 8,
     title: "Breath",
     year: "2024",
-    image: "./img/ff2ac3d3-fa94-4811-89f6-0d008b27e439.png",
+    image: "ff2ac3d3-fa94-4811-89f6-0d008b27e439",
   },
   {
     id: 9,
     title: "Stillness",
     year: "2025",
-    image: "./img/c39a4043-f489-4406-8018-a103a3f89802.png",
+    image: "c39a4043-f489-4406-8018-a103a3f89802",
   },
   {
     id: 10,
     title: "Surrender",
     year: "2025",
-    image: "./img/e5e399f2-4050-463b-a781-4f5a1615f28e.png",
+    image: "e5e399f2-4050-463b-a781-4f5a1615f28e",
   },
 ]
 
@@ -81,7 +81,7 @@ function initializeBackground(box) {
 
 function updateBackground(project, box) {
   if (activeBackgroundElement) {
-    activeBackgroundElement.style.backgroundImage = `url(${project.image})`
+    activeBackgroundElement.style.backgroundImage = `url(./img/${project.image}.jpg)`
 
     activeBackgroundElement.classList.remove("image--open")
     void activeBackgroundElement.offsetWidth
